bd16254d145a30b1cc8c633212488dba920e0b69
João Henrique Delucca Barros <jhdbarros@hotmail.com>
2019-10-21 23:31:15 -0300

    Includes the Hello World JS code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var esquerda = 37, cima = 38, direita = 39 , baixo = 40;  // inicializa as variaves com seus keyCode;
+var moveEsquerda = false, moveDireita = false, moveCima = false, moveBaixo = false;
 var canvas = document.querySelector("canvas")
 var cx = canvas.getContext("2d");
 var quadrado = {
@@ -6,33 +7,80 @@ var quadrado = {
 	y:10
 };
 
-renderizar();
+atualizar();
 
-window.addEventListener("keydown", manipulador); // funçao para quando apertar a tecla, fazer alguma coisa
+window.addEventListener("keydown", apertar); // funçao para quando apertar a tecla, fazer alguma coisa
 
-function manipulador(e) {
-	var chave = e.keyCode; // var chave recebe o codigo das teclas de direção
+window.addEventListener("keyup",soltar); //Dirspara um evento quando uma tecla é liberada
+
+function apertar(e) {
+	let chave = e.keyCode; // var chave recebe o codigo das teclas de direção
 	
-	if(chave == esquerda) {
-		quadrado.x--;
+	if(chave == esquerda && chave !== direita) {
+		moveEsquerda = true;
 	}
-	if(chave == direita) {
-		quadrado.x++;
+	if(chave == direita && chave !== esquerda) {
+		moveDireita = true;
 	}
-	if(chave == cima) {
-		quadrado.y--;
+	if(chave == cima && chave != baixo) {
+		moveCima = true;
 	}
-	if(chave == baixo) {
-		quadrado.y++;
+	if(chave == baixo && chave != cima) {
+		moveBaixo = true;
 	}
-	
-	renderizar();
 }
 
+function soltar(e){
+			//Variável recebe o código da tecla liberada
+			let chave = e.keyCode;
+			//Esquerda
+			if(chave == esquerda && chave !== direita){
+				moveEsquerda = false;
+			}
+			//Direita
+			if(chave == direita && chave !== esquerda){
+				moveDireita = false;
+			}
+			//Para Cima
+			if(chave == cima && chave !== baixo){
+				moveCima = false;
+			}
+			//Para Baixo
+			if(chave == baixo && chave !== cima){
+				moveBaixo = false;
+			}
+		}
 
+	function mover(){
+			
+			if(moveEsquerda){
+				quadrado.x--;
+			}
+			
+			if(moveDireita){
+				quadrado.x++;
+			}
+			
+			if(moveCima){
+				quadrado.y--;
+			}
+			
+			if(moveBaixo){
+				quadrado.y++;
+			}
+		}
 
-function renderizar() {
-	cx.clearRect(0,0,canvas.width,canvas.height);
-	cx.fillRect(quadrado.x,quadrado.y,50,50)
-}
+	function renderizar() {
+		cx.clearRect(0,0,canvas.width,canvas.height);
+		cx.fillRect(quadrado.x,quadrado.y,50,50)
+	}
+	
+	function atualizar(){
+			
+			requestAnimationFrame(atualizar, canvas);
+			
+			mover();
+			
+			renderizar();
+		}
 
